640439271199d8048f487cb034047b8c1a8387f2
Máté <t1uyl9@inf.elte.hu>
2026-03-19 16:04:32 +0100

    Rebased main onto master

--- Assets/00_Scripts/roads/RoadModel.cs
+++ Assets/00_Scripts/roads/RoadModel.cs
@@ -1,21 +1,38 @@
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Windows;
+using static UnityEditor.Rendering.CameraUI;
 
-public class RoadModel : MonoBehaviour
+public abstract class RoadModel : MonoBehaviour
 {
     [SerializeField]
     private Transform wrapper;
     public float Rotation => wrapper.transform.eulerAngles.y;
     private BuildingShapeUnit[] shapeUnits;
+    public abstract Direction[] Inputs {  get; }
+    public abstract Direction[] Outputs { get; }
     private void Awake()
     {
         shapeUnits = GetComponentsInChildren<BuildingShapeUnit>();
     }
     public void Rotate(float rotationStep)
     {
-        wrapper.Rotate(new(0, rotationStep, 0));
+        wrapper.Rotate(new Vector3(0, rotationStep, 0));
+
+        int steps = Mathf.RoundToInt(rotationStep / 90f);
+
+        for (int i = 0; i < Inputs.Length; i++) // rotates inputs
+        {
+            Inputs[i] = (Direction)(((int)Inputs[i] + steps) % 4);
+        }
+
+        for (int i = 0; i < Outputs.Length; i++) // rotates outputs
+        {
+            Outputs[i] = (Direction)(((int)Outputs[i] + steps) % 4);
+        }
     }
+
     public List<Vector3> GetAllBuildingPositions()
     {
         return shapeUnits.Select(unit => unit.transform.position).ToList();
---
+++ Assets/00_Scripts/roads/models/Direction.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public enum Direction
+{
+    N,
+    W,
+    S,
+    E
+}
